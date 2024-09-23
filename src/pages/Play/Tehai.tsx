7d46786d2ad39150828d36e8c8add60bc91d0056
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2024-09-23 21:18:50 +0900

    文言を修正

--- src/pages/Play/Tehai.tsx
+++ src/pages/Play/Tehai.tsx
@@ -123,7 +123,7 @@ export default function Tehai(props: tehaiProps) {
                       });
                     }}
                   >
-                    アガリ
+                    確認
                   </Button>
                 </>
               )}
