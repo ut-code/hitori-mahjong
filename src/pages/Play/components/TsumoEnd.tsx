6a196138987a482d8b8f923e296126efa069f5ac
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2024-11-04 17:05:49 +0900

    ツモ画面にCSSを設定

--- src/pages/Play/components/TsumoEnd.tsx
+++ src/pages/Play/components/TsumoEnd.tsx
@@ -1,5 +1,6 @@
 import Button from "@mui/material/Button";
 import React from "react";
+import styles from "../styles/TsumoEnd.module.css";
 
 type TsumoEndProps = {
   tsumoEnd: () => void;
@@ -7,11 +8,15 @@ type TsumoEndProps = {
 
 export default function TsumoEnd(props: TsumoEndProps) {
   return (
-    <>
-      <div>ツモ！！</div>
-      <Button variant="contained" onClick={props.tsumoEnd}>
+    <div className={styles.container}>
+      <div className={styles.title}>ツモ！！</div>
+      <Button
+        variant="contained"
+        onClick={props.tsumoEnd}
+        className={styles.checkButton}
+      >
         確認
       </Button>
-    </>
+    </div>
   );
 }
