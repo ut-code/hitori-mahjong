ec5af35e2524d4eaf6e6f1ad88b8718c0b3570d4
NISHIYAMA-Takuki <95198404+NISHIYAMA-Takuki@users.noreply.github.com>
2024-11-22 17:05:10 +0900

    Setがobjectを上手く扱えないことへの対応

--- src/utils/calculateSyantenToitsu.ts
+++ src/utils/calculateSyantenToitsu.ts
@@ -1,9 +1,18 @@
 import { Hai } from "./hai.ts";
 
 export default function calculateShantenToitsu(tehai: Hai[]): number {
-  const tehaiSet = new Set(tehai);
-  const haiNumber = [...tehaiSet].map(hai => tehai.filter(
-    h => (h.kind === hai.kind) && (h.value === hai.value)).length
+  const tehaiSet: Hai[] = [];
+  for (const hai of tehai) {
+    if (!tehaiSet.filter(v => sameHai(v, hai)).length) {
+      tehaiSet.push(hai);
+    }
+  }
+  const haiNumber = tehaiSet.map(hai => tehai.filter(
+    h => sameHai(hai, h)).length
   );
   return 6 - haiNumber.filter(n => n >= 2).length + Math.max(0, 7 - [...tehaiSet].length);
+}
+
+function sameHai(a: Hai, b: Hai): boolean {
+  return (a.kind === b.kind) && (a.value === b.value);
 }
