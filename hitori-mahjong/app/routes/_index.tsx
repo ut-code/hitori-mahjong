bffb197af1092cd0e7195d7040dda2cd4d0539f8
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-10-27 16:29:50 +0800

    format?

--- hitori-mahjong/app/routes/_index.tsx
+++ hitori-mahjong/app/routes/_index.tsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router";
 
 export default function Page() {
-  return (
-    <>
-      <h1 className="text-5xl text-center pb-1">Hitori Mahjong</h1>
-      <Link to="/start">
-        <p className="text-center link">Get Started</p>
-      </Link>
+	return (
+		<>
+			<h1 className="text-5xl text-center pb-1">Hitori Mahjong</h1>
+			<Link to="/start">
+				<p className="text-center link">Get Started</p>
+			</Link>
 
-      <Link to="/learn">
-        <p className="text-center link">Learn How to Play</p>
-      </Link>
-    </>
-  );
+			<Link to="/learn">
+				<p className="text-center link">Learn How to Play</p>
+			</Link>
+		</>
+	);
 }
