d2ba5f7f0c7a146a67446b83af959bea4390a45a
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-10-28 17:27:12 +0800

    format

--- rr/app/lib/auth.ts
+++ rr/app/lib/auth.ts
@@ -3,15 +3,15 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { getDB } from "./db";
 
 export function getAuth(env?: Env) {
-  const auth = betterAuth({
-    database: drizzleAdapter(getDB(env), {
-      provider: "pg",
-    }),
-    emailAndPassword: {
-      enabled: true,
-    },
-  });
-  return auth;
+	const auth = betterAuth({
+		database: drizzleAdapter(getDB(env), {
+			provider: "pg",
+		}),
+		emailAndPassword: {
+			enabled: true,
+		},
+	});
+	return auth;
 }
 
 export const auth = getAuth();
--- rr/app/routes/api.auth.$.ts
+++ rr/app/routes/api.auth.$.ts
@@ -2,13 +2,13 @@ import type { Route } from "./+types/api.auth.$";
 import { getAuth } from "~/lib/auth";
 
 export async function loader({ request, context }: Route.LoaderArgs) {
-  const { env } = context.cloudflare;
-  const auth = getAuth(env);
-  return auth.handler(request);
+	const { env } = context.cloudflare;
+	const auth = getAuth(env);
+	return auth.handler(request);
 }
 
 export async function action({ request, context }: Route.ActionArgs) {
-  const { env } = context.cloudflare;
-  const auth = getAuth(env);
-  return auth.handler(request);
+	const { env } = context.cloudflare;
+	const auth = getAuth(env);
+	return auth.handler(request);
 }
