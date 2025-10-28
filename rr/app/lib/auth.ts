97e57473ded609260f34d32eaa556686892004d8
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-10-28 17:26:57 +0800

    setup better auth

--- rr/app/lib/auth.ts
+++ rr/app/lib/auth.ts
@@ -2,10 +2,16 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { getDB } from "./db";
 
-export function getAuth(env: Env) {
+export function getAuth(env?: Env) {
   const auth = betterAuth({
     database: drizzleAdapter(getDB(env), {
       provider: "pg",
     }),
+    emailAndPassword: {
+      enabled: true,
+    },
   });
+  return auth;
 }
+
+export const auth = getAuth();
--- rr/app/routes/api.auth.$.ts
+++ rr/app/routes/api.auth.$.ts
@@ -1,22 +1,14 @@
-import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
-import { drizzle as drizzlePg } from "drizzle-orm/node-postgres";
 import type { Route } from "./+types/api.auth.$";
+import { getAuth } from "~/lib/auth";
 
 export async function loader({ request, context }: Route.LoaderArgs) {
-	const { env } = context.cloudflare;
-	const db =
-		env.NODE_ENV === "development"
-			? drizzlePg(env.DATABASE_URL)
-			: drizzleNeon(env.DATABASE_URL);
-	const auth = betterAuth({
-		database: drizzleAdapter(db, {
-			provider: "pg",
-		}),
-		emailAndPassword: {
-			enabled: true,
-		},
-	});
-	return auth.handler(request);
+  const { env } = context.cloudflare;
+  const auth = getAuth(env);
+  return auth.handler(request);
+}
+
+export async function action({ request, context }: Route.ActionArgs) {
+  const { env } = context.cloudflare;
+  const auth = getAuth(env);
+  return auth.handler(request);
 }
