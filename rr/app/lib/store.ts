050bb91a031ac06321da086a0d002cd8eb99bd9c
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-10-30 18:07:37 +0800

    format

--- rr/app/lib/store.ts
+++ rr/app/lib/store.ts
@@ -1,26 +1,26 @@
+import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import type { Hai } from "./hai";
-import { create } from "zustand";
 
 interface GameState {
-  kyoku: number;
-  junme: number;
-  haiyama: Hai[];
-  sutehai: Hai[];
-  tehai: Hai[];
+	kyoku: number;
+	junme: number;
+	haiyama: Hai[];
+	sutehai: Hai[];
+	tehai: Hai[];
 }
 
 const useGameState = create<GameState>()(
-  persist(
-    (set) => ({
-      kyoku: 1,
-      junme: 1,
-      haiyama: [],
-      sutehai: [],
-      tehai: [],
-    }),
-    {
-      name: "game-state-storage",
-    },
-  ),
+	persist(
+		(set) => ({
+			kyoku: 1,
+			junme: 1,
+			haiyama: [],
+			sutehai: [],
+			tehai: [],
+		}),
+		{
+			name: "game-state-storage",
+		},
+	),
 );
