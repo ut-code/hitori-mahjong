90e37a7e92517edd7b886817e15a4224c8c7f6dc
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-10-30 16:17:21 +0800

    format

--- rr/app/lib/auth.ts
+++ rr/app/lib/auth.ts
@@ -1,31 +1,31 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { getDB } from "./db";
 import { anonymous } from "better-auth/plugins";
 import { eq } from "drizzle-orm";
+import { getDB } from "./db";
 
 export function getAuth(env?: Env) {
-  const auth = betterAuth({
-    database: drizzleAdapter(getDB(env), {
-      provider: "pg",
-    }),
-    emailAndPassword: {
-      enabled: true,
-    },
-    plugins: [
-      anonymous({
-        onLinkAccount: async ({ anonymousUser, newUser }) => {
-          const db = getDB(env);
-          // migrate like this
-          // await db
-          //   .update(playHistory)
-          //   .set({ userId: newUser.user.id })
-          //   .where(eq(playHistory.userId, anonymousUser.user.id));
-        },
-      }),
-    ],
-  });
-  return auth;
+	const auth = betterAuth({
+		database: drizzleAdapter(getDB(env), {
+			provider: "pg",
+		}),
+		emailAndPassword: {
+			enabled: true,
+		},
+		plugins: [
+			anonymous({
+				onLinkAccount: async ({ anonymousUser, newUser }) => {
+					const db = getDB(env);
+					// migrate like this
+					// await db
+					//   .update(playHistory)
+					//   .set({ userId: newUser.user.id })
+					//   .where(eq(playHistory.userId, anonymousUser.user.id));
+				},
+			}),
+		],
+	});
+	return auth;
 }
 
 export const auth = getAuth();
