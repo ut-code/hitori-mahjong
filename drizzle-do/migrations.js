38b14bfc37a0a8b77a51f8d885c7f67cdcd53860
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-12-26 21:15:18 +0900

    format

--- drizzle-do/migrations.js
+++ drizzle-do/migrations.js
@@ -1,14 +1,13 @@
-import journal from './meta/_journal.json';
-import m0000 from './0000_bright_dark_phoenix.sql?raw';
-import m0001 from './0001_awesome_brood.sql?raw';
-import m0002 from './0002_chilly_changeling.sql?raw';
+import m0000 from "./0000_bright_dark_phoenix.sql?raw";
+import m0001 from "./0001_awesome_brood.sql?raw";
+import m0002 from "./0002_chilly_changeling.sql?raw";
+import journal from "./meta/_journal.json";
 
-  export default {
-    journal,
-    migrations: {
-      m0000,
-m0001,
-m0002
-    }
-  }
-  
+export default {
+	journal,
+	migrations: {
+		m0000,
+		m0001,
+		m0002,
+	},
+};
