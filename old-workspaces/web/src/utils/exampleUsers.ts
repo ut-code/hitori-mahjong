a6910e83bd56e6b23921d5b6c853baacdc1830bd
tknkaa <tknkaa@users.noreply.github.com>
2026-03-11 22:15:12 +0800

    chore: remove legacy application and dev setup

--- old-workspaces/web/src/utils/exampleUsers.ts
+++
@@ -1,6 +0,0 @@
-export const exampleUsers = [
-	{ rank: 0, name: "sample-1", score: -54500 },
-	{ rank: 0, name: "sample-2", score: -54500 },
-	{ rank: 0, name: "sample-3", score: -54500 },
-	{ rank: 0, name: "sample-4", score: -54500 },
-];
