a4c6d2543a0da59640aa12e6e3bbd7d00a7c3ba4
tknkaa <tknkaa@users.noreply.github.com>
2026-04-13 21:22:01 +0800

    fix

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,9 +1,9 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { anonymous } from "better-auth/plugins";
-import Database from "better-sqlite3";
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import { findLocalD1Database } from "../../find-local-d1";
+// import Database from "better-sqlite3";
+// import { drizzle } from "drizzle-orm/better-sqlite3";
+// import { findLocalD1Database } from "../../find-local-d1";
 import * as schema from "../lib/db/schema";
 import { getDB } from "./db";
 
@@ -18,13 +18,14 @@ export function getAuth(env: Env) {
 	return auth;
 }
 
-const path = findLocalD1Database();
-if (!path) throw new Error("Cannot find local d1");
-
-export const auth = betterAuth({
-	database: drizzleAdapter(drizzle(new Database(path), { schema }), {
-		provider: "sqlite",
-		schema: schema,
-	}),
-	plugins: [anonymous()],
-});
+// for better-auth cli
+// const path = findLocalD1Database();
+// if (!path) throw new Error("Cannot find local d1");
+//
+// export const auth = betterAuth({
+// 	database: drizzleAdapter(drizzle(new Database(path), { schema }), {
+// 		provider: "sqlite",
+// 		schema: schema,
+// 	}),
+// 	plugins: [anonymous()],
+// });
